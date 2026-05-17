5d7fefef08bf4b3b081e05ee7ef2335bce1a6d1b
thefex <przemyslawraciborski@gmail.com>
2026-05-17 19:22:25 +0200

    Sample app

--- AdvancedRecyclerView/Sample/Sample.Android/Adapters/Expandable/SampleGroupedDataConverter.cs
+++ AdvancedRecyclerView/Sample/Sample.Android/Adapters/Expandable/SampleGroupedDataConverter.cs
@@ -5,13 +5,25 @@ namespace Sample.Android.Adapters.Expandable;
 
 public class SampleGroupedDataConverter : MvxExpandableDataConverter
 {
+    Dictionary<string, long> _nameToGroupIdMap = new Dictionary<string, long>();
+    private long currentId;
     public override MvxGroupedData ConvertToMvxGroupedData(object item)
     {
+        var groupId = 0l;
+            
         var group = (CategoryGroup)item;
+        if (_nameToGroupIdMap.ContainsKey(group.Name))
+            groupId = _nameToGroupIdMap[group.Name];
+        else
+        {
+            groupId = currentId++;
+            _nameToGroupIdMap[group.Name] = groupId;
+        }
+        
         return new MvxGroupedData
         {
             Key = group.Name,
-            UniqueId = group.Name.GetHashCode(),
+            UniqueId = groupId,
             GroupItems = group.Items,
         };
     }
